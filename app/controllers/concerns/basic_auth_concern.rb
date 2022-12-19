5aa1e1fe24cb7eada7bd86851a6c635e30911816
Matthew Davis <matc.davis@gmail.com>
2022-12-19 03:53:37 +0000

    more testing with ENV variables, dot.env is available, only needed to set varaible before passing to heep basic auth

--- app/controllers/concerns/basic_auth_concern.rb
+++ app/controllers/concerns/basic_auth_concern.rb
@@ -1,11 +1,14 @@
 module BasicAuthConcern 
   extend ActiveSupport::Concern
+  
   included do
     before_action :http_authenticate
   end
   
   def http_authenticate
-    p ENV["ADMIN_USERNAME"]
-    # http_basic_authenticate_or_request_with(name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'])
+    username = ENV['ADMIN_USERNAME']
+    password = ENV['ADMIN_PASSWORD']
+    
+    http_basic_authenticate_or_request_with(name: username, password: password)
   end
 end
