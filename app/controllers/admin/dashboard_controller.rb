eedeeaeadea0e0ae3356f94c9a8876167c59d73c
Matthew Davis <matc.davis@gmail.com>
2022-12-19 19:33:33 +0000

    reworking controller to display totals as well as counts by category

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,6 @@ class Admin::DashboardController < Admin::BaseController
   def show
     @product_count = Product.count
     @cat_count = Category.count
-    @cat = Category.all
+    @product_by_cat = Category.joins(:products).group(:name).count
   end
 end
