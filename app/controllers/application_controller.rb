b547986f689a91d011988ff7129deb85ab799263
Matthew Davis <matc.davis@gmail.com>
2022-12-20 23:44:55 +0000

    working on forms for login/signup add comment about authorize method that is likely not going to be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,12 +19,21 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  # Add a before_filter to any controller that you want to secure. This will force user's to login before they can see the actions in this controller. I've created a gif controller below which I'm going to secure. The routes for this controller were added to the routes.rb in the beginning of this tutorial.
+  def authorize
+    redirect_to '/login' unless current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    # render json: params
+    @user = User.where("email = params[:e_mail]")
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
