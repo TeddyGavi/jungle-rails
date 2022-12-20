0f3249cbb21b013819a9c06cf6a0131ddc1e5c19
Matthew Davis <matc.davis@gmail.com>
2022-12-20 17:49:17 +0000

    testing a delete functionality and adding a validation for name when making a new category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,15 @@ class Category < ApplicationRecord
 
   has_many :products
 
+  validates :name, presence: true
+
+  before_destroy :has_products, prepend: true
+
+  def has_products?
+    if self.products.count >= 1
+      errors.add(:product, "Cannot delete a category that contains products, considering renaming?") 
+      throw :abort
+    end
+  
+  end
 end
