4d2d0a498c8da7ff45cbb7a1aa14970c53d16c5c
Matthew Davis <matc.davis@gmail.com>
2023-01-06 20:56:12 +0000

    found duplicate presence validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :e_mail, uniqueness: { case_sensitive: false }, presence: true
-  validates :password, length: { minimum: 3 }, presence: true
+  validates :password, length: { minimum: 3 } 
 
 end
