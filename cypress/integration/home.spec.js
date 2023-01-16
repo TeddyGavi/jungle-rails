8318402bf8dea8a419b107bfa417450e30c6a735
Matthew Davis <matc.davis@gmail.com>
2023-01-16 21:11:04 +0000

    finalize testing of home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,9 +3,13 @@
 describe("it should be able to visit the home page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
+  });
 
-    it("Should show products on the page", () => {
-      cy.get(".products article").should("be.visible");
-    });
+  it("Should show products on the home page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
   });
 });
